90d74e669fdcab9033d36eaedf7803e8b5982605
Axel Gontier <axel.gontier.tra@cebi.com>
2022-08-03 10:55:40 +0200

    add waiter

--- main.py
+++ main.py
@@ -27,34 +27,38 @@ driver=webdriver.Firefox(options=options,executable_path="geckodriver.exe")
 filename=st.file_uploader("Upload a picture",type=["png","jpg"])
 if st.button('Find name'):
     if filename is not None:
-        image2 = Image.open(filename)
-        image2.save("pic.png")
-        st.image(image2,caption="Picture to predict")
-         
+        with st.spinner('Searching...'):
+            
 
-        filePath = 'pic.png'
-        searchUrl = 'http://www.google.hr/searchbyimage/upload'
-        multipart = {'encoded_image': (filePath, open(filePath, 'rb')), 'image_content': ''}
-        response = requests.post(searchUrl, files=multipart, allow_redirects=False)
+            image2 = Image.open(filename)
+            image2.save("pic.png")
+            st.image(image2,caption="Picture to predict")
+            
 
-        fetchUrl = response.headers['Location']
-        print(fetchUrl)
+            filePath = 'pic.png'
+            searchUrl = 'http://www.google.hr/searchbyimage/upload'
+            multipart = {'encoded_image': (filePath, open(filePath, 'rb')), 'image_content': ''}
+            response = requests.post(searchUrl, files=multipart, allow_redirects=False)
 
-        driver.get(fetchUrl)
+            fetchUrl = response.headers['Location']
+            print(fetchUrl)
 
-        input=driver.find_element(By.TAG_NAME,"input")
-        name=input.get_attribute("value")
-        new_name=name +" site:stockx.com"
-        print(new_name)
-        driver.execute_script(f"document.getElementsByTagName('input')[0].setAttribute('value','{new_name}')") # add filter  site:stockx.com
-        driver.find_element(By.ID,"L2AGLb").click()#click cookies
-        driver.find_element(By.CLASS_NAME,"zgAlFc").click() #click search button 
+            driver.get(fetchUrl)
 
-        res=driver.find_element(By.TAG_NAME,"h3")
-        res=res.text.split("-")[0]
+            input=driver.find_element(By.TAG_NAME,"input")
+            name=input.get_attribute("value")
+            new_name=name +" site:stockx.com"
+            print(new_name)
+            driver.execute_script(f"document.getElementsByTagName('input')[0].setAttribute('value','{new_name}')") # add filter  site:stockx.com
+            driver.find_element(By.ID,"L2AGLb").click()#click cookies
+            driver.find_element(By.CLASS_NAME,"zgAlFc").click() #click search button 
+
+            res=driver.find_element(By.TAG_NAME,"h3")
+            res=res.text.split("-")[0]
         st.success(f"Name is : {res}")
     else:
          st.error("Please update a picture")
+         
     
 
 
